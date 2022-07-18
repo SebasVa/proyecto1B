b3a5713bdeb24644540ba012671ce46fe13ed32c
Mateo Cevallos <66786471+matojo074@users.noreply.github.com>
2022-07-18 00:18:46 -0500

    Actualizaciones 2.0

--- src/app/services/auth.service.ts
+++ src/app/services/auth.service.ts
@@ -1,9 +1,47 @@
 import { Injectable } from '@angular/core';
+import {User} from "../Shared/user.interface";
+import { AngularFireAuth} from '@angular/fire/compat/auth';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+  constructor(private afAuth:AngularFireAuth) { }
+
+  
+  async register(email:string, password:string):Promise<User>{
+    try{
+      const{ user } = await this.afAuth.createUserWithEmailAndPassword(email, password);
+      await this.sendVerificacionEmail();
+      return user;
+    }catch(error) {
+      console.log('Error -> ', error);
+    }
+  }
+
+  async sendVerificacionEmail(): Promise<void> {
+    try{
+      return (await this.afAuth.currentUser).sendEmailVerification();
+    } catch (error){
+      console.log('Error -> ', error);
+    }
+  }
+
+  async login(email:string, password:string):Promise<User>{
+    try{
+      const {user} = await this.afAuth.signInWithEmailAndPassword(email, password);
+      return user;
+    }catch(error) {
+      console.log('Error -> ', error);
+    }
+  }
+
+  async logout():Promise<void>{
+    try{
+      await this.afAuth.signOut();
+    } catch (error){
+      console.log('Error ->', error);
+    }
+  }
 }
